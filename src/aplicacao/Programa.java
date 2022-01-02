0fa61413cd508732e768d8e659a4617b3d55ffe2
pauloricardolongi <pauloricardolongi@gmail.com>
2022-01-02 03:11:34 -0300

    lista de origem vai ser uma lista de qualquer tipo numerico extends Number.  A lista de destino pode ser qualquer tipo que pode ser um supertipo de Numbe

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -1,37 +1,36 @@
 package aplicacao;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
-
-import entidades.Circulo;
-import entidades.Retangulo;
-import entidades.Shape;
 
 public class Programa {
 
 	public static void main(String[] args) {
-		List<Shape> myShapes = new ArrayList<>();
-		myShapes.add(new Retangulo(3.0, 2.0));
-		myShapes.add(new Circulo(2.0));
 		
-		List<Circulo>meusCirculos = new ArrayList<Circulo>();
-		meusCirculos.add(new Circulo(2.0));
-		meusCirculos.add(new Circulo(3.0));
+		List<Integer> myInts = Arrays.asList(1, 2, 3, 4);
+		List<Double> myDoubles = Arrays.asList(3.14, 6.28, 23.14);
+		List<Object> myObjs = new ArrayList<Object>();
 		
-		
-		
-		System.out.println("Total area: " + totalArea(meusCirculos));
+		copy(myInts, myObjs);
+		printList(myObjs);
+		copy(myDoubles, myObjs);
+		printList(myObjs);
+
 	}
-	//com coringa delimitado não podemos adicionar elementos no método
-	
-	public static double totalArea(List<? extends Shape> list) {
-		list.add(new Circulo(2.3, 4.0));
-		double sum = 0.0;
-		for (Shape s : list) {
-			sum = sum + s.area();
+	/*lista de origem vai ser uma lista de qualquer tipo numerico extends Number.
+	 A lista de destino pode ser qualquer tipo que pode ser um supertipo de Number*/
+	public static void copy(List<? extends Number> source, List<? super Number> destino) {
+		for(Number number : source) {
+			destino.add(number);
 		}
-		return sum;
+	}
+	public static void printList(List<?>list) {
+		for(Object obj : list) {
+			System.out.print(obj + " ");
 		}
+		System.out.println();
+	}
 
 	}
 
